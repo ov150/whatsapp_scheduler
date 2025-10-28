103585b952edb8b93a15ad611f46912f9dc56fd6
ov150 <ov5010143@gmail.com>
2025-10-28 11:00:31 +0530

    just try to mcp server in vscode

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pywhatkit
 import pyautogui
 import time
 from dotenv import load_dotenv
+from datetime import datetime
 import os
 
 load_dotenv()
@@ -10,16 +11,35 @@ load_dotenv()
 phone_number = os.getenv("PHONE_NUMBER")
 
 # Message
-message = os.getenv("MESSAGE")
+current_hour = datetime.now().hour
 
+# Decide message based on time
+if 5 <= current_hour < 12:
+    message = "🌅 Good Morning!"
+elif 12 <= current_hour < 17:
+    message = "☀️ Good Afternoon!"
+elif 17 <= current_hour < 21:
+    message = "🌇 Good Evening!"
+else:
+    message = "🌙 Good Night!"
+
+print(f"Sending message: {message}")
 # Time in 24-hour format
-hour = int(os.getenv("HOUR"))
-minute = int(os.getenv("MINUTE"))
+# hour = int(os.getenv("HOUR"))
+# minute = int(os.getenv("MINUTE"))
 
+
+send_hour = datetime.now().hour
+send_minute = datetime.now().minute + 1
+if send_minute >= 60:
+    send_hour = (send_hour + 1) % 24
+    send_minute = 0
+    
 # Schedule message
-pywhatkit.sendwhatmsg(phone_number, message, hour, minute)
+pywhatkit.sendwhatmsg(phone_number, message, send_hour, send_minute,15,True,3)
 time.sleep(15)
 pyautogui.press("enter")
 
 print("✅ Message scheduled successfully! Wait for WhatsApp Web to open.")
+
 
